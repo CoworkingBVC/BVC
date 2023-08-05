add4c8639a96a7bc1bca705a0db4d0c40013cbda
jiwonjeon <j.jeon339@mybvc.ca>
2023-08-05 11:59:49 -0600

    add comments

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/* for each page? */
+/* 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+ */
+
+/* main */
+//POST - login
+
+/* signup */
+//POST - signup
+
+/* coworker */
+//GET - display workspace
+//POST - booking
+
+/* owner */
+//GET - display owner's properties
+//POST - add a new property
+//POST or PATCH -  edit a property
+//DELETE - delete a property
+
 app.listen(8081, () => {
   console.log("Example app listening on port 8081!");
 });
