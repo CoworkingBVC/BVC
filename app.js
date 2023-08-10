c57c32f388542cc219f9d38c2bea840035843532
Jhenyffer Cristo <jhenyffercristo@gmail.com>
2023-08-09 18:01:08 -0600

    Add owner POST and GET

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -75,11 +76,82 @@ app.get("/coworker/:id", (req, res) => {
 });
 
 /* owner -jhenyffer*/
-//GET - display owner's properties
 //POST - add a new property
-//POST or PATCH -  edit a property
+//GET - display a property
+//GET - display owner's properties
+//PUT or PATCH -  edit a property
 //DELETE - delete a property
 
-app.listen(8081, () => {
-  console.log("Example app listening on port 8081!");
+//POST - add a new property
+app.post("/properties", (req, res) => {
+  if (!req.query.ownerId) {
+    res.status(400).send();
+  }
+  const ownerProperty = {
+    propertyId: uuidv4(), //Library that generate IDs
+    ownerId: req.query.ownerId,
+    address: req.body.address,
+    neighborhood: req.body.neighborhood,
+    squareFeet: req.body.squareFeet,
+    hasParking: req.body.hasParking,
+    hasPublicTransit: req.body.hasPublicTransit,
+    workspace: req.body.workspace, //workspace: Array of Objects with all the options
+  };
+  const propertyDB = readPropertyData();
+  propertyDB.push(ownerProperty);
+  savePropertyData(propertyDB);
+  res.status(200).send({ ownerProperty });
+});
+
+function readPropertyData() {
+  try {
+    const data = fs.readFileSync("properties.json", "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+  }
+}
+
+function savePropertyData(data) {
+  try {
+    fs.writeFileSync("properties.json", JSON.stringify(data, null, 2));
+    console.log("Property data saved to properties.json");
+  } catch (error) {
+    console.error("Error writing JSON file:", error);
+  }
+}
+//GET - display a property
+app.get("/properties/:id", (req, res) => {
+  const ownerPropertyData = readPropertyData();
+  let propertyFound;
+  ownerPropertyData.forEach((property) => {
+    if (property.propertyId === req.params.id) {
+      propertyFound = property;
+    }
+  });
+  if (propertyFound) {
+    res.status(200).send(propertyFound);
+  } else {
+    res.status(404).send();
+  }
+});
+
+//GET - display owner's properties
+app.get("/properties", (req, res) => {
+  if (!req.query.ownerId) {
+    res.status(400).send();
+  }
+  const ownerPropertyData = readPropertyData();
+  const ownerProperties = [];
+  ownerPropertyData.forEach((property) =>{
+    if(property.ownerId === req.query.ownerId){
+      ownerProperties.push(property);
+    }
+  })
+  res.status(200).send(ownerProperties);
+});
+//PUT or PATCH -  edit a property
+
+app.listen(8084, () => {
+  console.log("Example app listening on port 8084!");
 });
